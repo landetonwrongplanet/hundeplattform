f4e3a489463b324e2313db8810f900388d5297a2
landetonwrongplanet <mail@ileniacastano.ch>
2022-11-01 14:52:18 +0100

    html code

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Hundeplattform</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <div class="logo"><img src="logo.jpeg" alt="Dog on computer" width="180" height="200" class="logo"></div>
+        <div class="sitetitle"><p class="title">Hundeplattform</p></div>
+      </div>
+
+    </div>
+  </body>
+</html>
