7da9d13cfc57e41969291855834916cb204b9fe3
landetonwrongplanet <mail@ileniacastano.ch>
2023-01-25 11:20:16 +0100

    css changes

--- user/index.php
+++ user/index.php
@@ -16,71 +16,51 @@ session_start();
         <div class="logo"><img src="logo.jpeg" alt="Dog on computer" width="180" height="200" class="logo"></div>
         <div class="sitetitle"><p class="title">Hundeplattform</p></div>
       </div>
-      <div class="landing">
-      <div class="forms">
-        <div class="registration">
-        <div data-role="main" class="ui-content">
-          <p>Registration</p>
-          <form data-ajax="false" id="loginformular" method="post"
-              action="user.php">
-              <div data-role="fieldcontain">
-                <fieldset>
-                  <label for="surname">Vorname</label>
-                  <input type="text" name="surname" id="s-name" focus><br>
-                  <label for="lastname">Nachname</label>
-                  <input type="text" name="lastname" id="l-name" focus><br>
-                  <label for="username">Benutzername</label>
-                  <input type="text" name="username" id="username" focus><br>
-                  <label for="email">E-Mail</label>
-                  <input type="email" name="email" id="email" focus><br>
-                  <label for="password">Kennwort</label>
-                  <input type="password" name="password1" id="password1"><br>
-                  <label for="password2">Kennwort wiederholen</label>
-                  <input type="password" name="password2" id="password2"><br>
-                  <input type="submit" value="register">
-                </fieldset>
-              </div>
-          </form>
+        <div class="landing">
+            <div class="forms">
+              <div class="registration">
+              <p>Registration</p>
+                <form class="loginformular" method="post"action="user.php">
+                        <label for="surname">Vorname</label><br>
+                        <input type="text" name="surname" id="s-name" focus><br>
+                        <label for="lastname">Nachname</label><br>
+                        <input type="text" name="lastname" id="l-name" focus><br>
+                        <label for="username">Benutzername</label><br>
+                        <input type="text" name="username" id="username" focus><br>
+                        <label for="email">E-Mail</label><br>
+                        <input type="email" name="email" id="email" focus><br>
+                        <label for="password">Kennwort</label><br>
+                        <input type="password" name="password1" id="password1"><br>
+                        <label for="password2">Kennwort wiederholen</label><br>
+                        <input type="password" name="password2" id="password2"><br>
+                        <input type="submit" value="register">
+                </form>
+            </div>
+            <div class="login">
+              <p>Login</p>
+                <form action="user.php" method="post">
+                  <label for="surname">Vorname</label><br>
+                  <input type="text" name="vorname"><br>
+                  <label for="lastname">Nachname</label><br>
+                  <input type="text" name="nachname"><br>
+                  <input type="submit" value="login" name="submit">
+                </form>
+            </div>
+          </div>
+      <div class="intro">
+        <p class="landingtext">
+            Hallo! Sie scheinen eine interessierte Hundeperson zu sein. 
+            Hier sind Sie auf jeden Fall am richtigen Ort. 
+            Nach der erfolgreichen Registrierung, 
+            können Sie sich einloggen um von unserem Datenbankservice vollumfänglich zu profitieren.
+            Sie können zu Hunderassen nachlesen, 
+            Bilder zu den jeweiligen Rassen ansehen und sich via Quiz die passende Rasse heraussuchen.
+        </p>
       </div>
+    </div>
+      <div class="footer">
+        <p class="author">Author: Beth Williams, Ilenia Castano</p>
       </div>
-        <div class="login">
-        <div data-role="main">
-          <p>Login</p>
-          <form data-ajax="false" id="loginformular" method="post"
-              action="user.php">
-              <div data-role="fieldcontain">
-                <fieldset>
-                  <label for="username">Benutzername</label>
-                  <input type="text" name="username" id="username" focus><br>
-                  <label for="password">Kennwort</label>
-                  <input type="password" name="password" id="password"><br>
-                  <input type="submit" value="login">
-                </fieldset>
-              </div>
-          </form>
-        </div>
-        </div>
-    </div>
-    <div class="intro">
-      <p class="landingtext">
-        Hallo! Sie scheinen eine interessierte Hundeperson zu sein. 
-        Hier sind Sie auf jeden Fall am richtigen Ort. 
-        Nach der erfolgreichen Registrierung, 
-        können Sie sich einloggen um von unserem Datenbankservice vollumfänglich zu profitieren.
-        Sie können zu Hunderassen nachlesen, 
-        Bilder zu den jeweiligen Rassen ansehen und sich via Quiz die passende Rasse heraussuchen.
-      </p>
-      <form action="user.php" method="post">
-        <input type="text" name="vorname">
-        <input type="text" name="nachname">
-        <input type="submit" value="register" name="submit">
-      </form>
-    </div>
-</div>
-<div class="footer">
-  <p>Author: Beth Williams, Ilenia Castano</p>
-  
-</div>
-  </div>
+    </div> 
   </body>
 </html>
---
+++ user/useraction.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Hundeplattform</title>
+  </head>
+  <body>
+  <div class="container">
+      <div class="header">
+        <div class="logo"><img src="logo.jpeg" alt="Dog on computer" width="180" height="200" class="logo"></div>
+        <div class="sitetitle"><p class="title">Hundeplattform</p></div>
+        <div class="navigation">
+        <nav>
+            <ul>
+                <li><a href="index.php">Zur Homepage</a></li>
+            </ul>
+        </nav>
+        </div>
+      </div>
+      <div class="intro">
+      <p class="landingtext">
+       Willkommen im privaten Bereich.
+       Hier können Sie alles zu verschiedenen Hunderassen erfahren. 
+      </p>
+    </div>
+    <div class="input">
+        <p>Suchen</p>
+
+    </div>
+    <div class="output">
+        <p>Ergebnisse</p>
+    </div>
+</div>
+  </body>
+</html>
